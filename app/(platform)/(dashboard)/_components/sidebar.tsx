6bcc950fc63f68c0d6db5014cb57d8fdff2bf4fa
gsdeoliveira <gabriielsousa2002@gmail.com>
2024-03-31 11:17:43 -0300

    server actions

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -7,7 +7,6 @@ import { useLocalStorage } from 'usehooks-ts'
 import { useOrganization, useOrganizationList } from '@clerk/nextjs'
 
 import { Button } from '@/components/ui/button'
-import { Separator } from '@/components/ui/separator'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Accordion } from '@/components/ui/accordion'
 import { NavItem, Organization } from './nav-item'
@@ -49,7 +48,15 @@ export const Sidebar = ({ storageKey = 't-sidebar-state' }: SidebarProps) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%] " />
+          <Skeleton className="h-10 w-10 " />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     )
   }
